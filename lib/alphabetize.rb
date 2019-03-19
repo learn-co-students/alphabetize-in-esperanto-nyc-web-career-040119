1357dd9d5eef8a13825142a661c0c476c8691205
Edgar Ong <emailedgar@gmail.com>
2019-03-19 12:51:11 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# ALPHABET[0..-1]
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.split("").map do |char|
+      ALPHABET.index(char)
+    end #phrase
+    # binding.pry
+  end #arr
 end
